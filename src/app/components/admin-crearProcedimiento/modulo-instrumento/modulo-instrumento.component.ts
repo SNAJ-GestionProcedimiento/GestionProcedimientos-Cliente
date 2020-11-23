9d7e20a17b8cf2ef6fb53f44b01b600780a07241
nato98 <natyorozco@unicauca.edu.co>
2020-11-23 16:23:38 -0500

    agregados componentes de procedimiento, instrumentos y especialidades en la creacion de procedimiento como admin

--- src/app/components/admin-crearProcedimiento/modulo-instrumento/modulo-instrumento.component.ts
+++ src/app/components/admin-crearProcedimiento/modulo-instrumento/modulo-instrumento.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ConfirmationDialogComponent } from 'src/app/confirmation-dialog/confirmation-dialog.component';
+import { editInstrumentosEquipos, InstrumentosEquipos } from 'src/_models/modelInstrumento/instrumentos-equipos.model';
+import { InstrumentosEquiposService } from 'src/_services/serviciosInstrumentos/instrumentos-equipos.service';
+import { UtilityServiceService } from 'src/_services/utility-service.service';
+import * as notificationService from 'src/_services/notification.service';
 
 @Component({
   selector: 'app-modulo-instrumento',
@@ -7,9 +15,155 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuloInstrumentoComponent implements OnInit {
 
-  constructor() { }
+  data: Array<any>;
+  arrayInstrumentos: InstrumentosEquipos[];
+  opcionSeleccionado: string = '0';
+  verSeleccion = '';
+  datosSeleccionador: InstrumentosEquipos[] = [];
+  editInstrument: editInstrumentosEquipos;  //variable utilizada para editar los 
+  datosAdd: InstrumentosEquipos[] = [];
+  idProcedimiento: number;
+
+  displayedColumns: string[] = ['codigo', 'nombre', 'cantidad', 'descripcion', 'acciones'];
+  dataSource = new MatTableDataSource<InstrumentosEquipos>();
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  constructor(
+    private serviceIntrumentosEquipos: InstrumentosEquiposService, 
+    private notificationService: notificationService.NotificationService,
+    private dialogo: MatDialog, 
+    private utilityService: UtilityServiceService
+    ) { }
 
   ngOnInit(): void {
+    this.getAllInstrumentos();
+    this.utilityService.customInstrumentoAdd.subscribe(msg => this.datosAdd = msg);
+    this.utilityService.customIdAgendaProcedimiento.subscribe(msg => this.idProcedimiento = msg);
+
   }
 
+  getAllInstrumentos() {
+    this.serviceIntrumentosEquipos.getAllIntrumentos().subscribe((result: InstrumentosEquipos[]) => {
+
+      this.arrayInstrumentos = InstrumentosEquipos.fromJSON(result);
+      //console.log("desde añadir instrumento: " + this.arrayInstrumentos);
+      if (this.arrayInstrumentos != null) {
+        for (let i = 0; i < this.arrayInstrumentos.length; i++) {
+          this.arrayInstrumentos[i].cantidad = 1;
+        }
+        //ordena el array por nombre
+        this.arrayInstrumentos.sort(function (a, b) {
+          return ((a.nombre < b.nombre) ? -1 : ((a.nombre > b.nombre) ? 1 : 0));
+        })
+      } else {
+        this.notificationService.success('No hay instrumentos y/o equipos en la base de datos');
+      }
+    });
+  }
+
+  capturar() {
+    this.verSeleccion = this.opcionSeleccionado;
+    this.agregarDatoTabla();
+  }
+
+  agregarDatoTabla() {
+    for (let i = 0; i < this.arrayInstrumentos.length; i++) {
+      if (this.arrayInstrumentos[i].nombre == this.verSeleccion) {
+        if (!this.datosSeleccionador.includes(this.arrayInstrumentos[i])) {
+          this.datosSeleccionador.push(this.arrayInstrumentos[i]);
+          this.dataSource = new MatTableDataSource(this.datosSeleccionador);
+          this.dataSource.paginator = this.paginator;
+          break;
+        }
+      }
+    }
+  }
+
+  confirmacionLimpiar() {
+    this.dialogo
+      .open(ConfirmationDialogComponent, {
+        data: `¿Seguro que desea eliminar TODOS los intrumentos y/o equipos que ha agregado en la tabla?`
+      })
+      .afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (confirmado) {
+          this.limpiarLista();
+        }
+      });
+  }
+
+  limpiarLista() {
+    this.datosSeleccionador = [];
+    this.dataSource = new MatTableDataSource(this.datosSeleccionador);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  eliminarDato(datoAEliminar: InstrumentosEquipos) {
+    this.dialogo
+      .open(ConfirmationDialogComponent, {
+        data: `¿Seguro que desea ELIMINAR el intrumentos y/o equipos de la lista?`
+      })
+      .afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (confirmado) {
+          for (let i = 0; i < this.datosSeleccionador.length; i++) {
+            if (this.datosSeleccionador[i] == datoAEliminar) {
+              this.datosSeleccionador.splice(i, 1);
+              this.dataSource = new MatTableDataSource(this.datosSeleccionador);
+              this.dataSource.paginator = this.paginator;
+              break;
+            }
+          }
+        }
+      });
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+  /*cerrarVentana() {
+    this.dialogo
+      .open(ConfirmationDialogComponent, {
+        data: `¿Seguro que desea SALIR de la ventana añadir intrumentos y/o equipos puesto que si sale y tiene instrumentos en la vista previa estos no se añadirán y se borrará de la tabla?`
+      })
+      .afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (confirmado) {
+          this.dialogo.closeAll();
+        }
+      });
+  }
+
+  //método para agregar instrumentos
+  addInstruments() {
+    this.dialogo
+      .open(ConfirmationDialogComponent, {
+        data: `¿Seguro que desea añadir todos los intrumentos y/o equipos que ha agregado en la tabla?`
+      })
+      .afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (confirmado) {
+          for (let i = 0; i < this.datosSeleccionador.length; i++) {
+            const resultado = this.datosAdd.find( intrumento => intrumento.codigoEquipo === this.datosSeleccionador[i].codigoEquipo );
+            if(!resultado){  //si no esta el instrimento al agregar en la lista de la agenda, se agrega
+              this.editInstrument = new editInstrumentosEquipos(this.datosSeleccionador[i].id, this.idProcedimiento, this.datosSeleccionador[i].codigoEquipo.toString(), "PEND", this.datosSeleccionador[i].cantidad);
+              let res = this.serviceIntrumentosEquipos.addInstrumento(this.editInstrument).subscribe();
+            }else{ //si esta el instrimento al agregar en la lista de la agenda, se edita
+              this.editInstrument = new editInstrumentosEquipos(resultado.id, this.idProcedimiento, resultado.codigoEquipo.toString(), "PEND", parseInt(resultado.cantidad.toString())+1);
+              let res = this.serviceIntrumentosEquipos.editarInstrumentoEquipo(this.editInstrument).subscribe();
+            }
+            this.utilityService.changeIntrumentoAdd(this.datosAdd);
+          }
+          this.utilityService.changeIntrumentoAdd(this.datosAdd);
+          this.dialogo.closeAll();
+        }
+      });
+
+  }*/
+
 }
+
--- src/app/components/admin-crearProcedimiento/modulo-procedimiento/modulo-procedimiento.component.ts
+++ src/app/components/admin-crearProcedimiento/modulo-procedimiento/modulo-procedimiento.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadoCama } from 'src/_models/estado-cama.model';
+import { Procedimiento } from 'src/_models/procedimiento.model';
+import { EstadoCamaService } from 'src/_services/estado-cama.service';
+import { ProcedimientoService } from 'src/_services/procedimiento.service';
+import { EditarComponentesService } from 'src/_services/serviciosComponentes/editar-componentes.service';
+import { UtilityServiceService } from 'src/_services/utility-service.service';
 
 @Component({
   selector: 'app-modulo-procedimiento',
@@ -7,9 +14,226 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuloProcedimientoComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+   /**Variables de busqueda autocompletada */
+   keyword = 'name';
+   data: Array<any>;
+ 
+   /**Variable formulario */
+   public busquedaForm: FormGroup;
+   public filtroBusqueda: string = '';
+   public valorBusqueda: string = '';
+   public codigoProc: string = '';
+   public estadoCama: string='';
+ 
+   /**Valriables de control */
+   public inputInactivo: string = "false";
+   public filtroSeleccionado: boolean = true;
+   public inputVacio: boolean = true;
+   public busquedaNula: boolean = true;
+   public msjBusquedaNula: string = "No existe procedimiento con ";
+ 
+   /**Valiables de peticiones */
+   public procedimientos: Array<Procedimiento>;
+   public procedimiento: Procedimiento = new Procedimiento();
+   public estadosCama: Array<EstadoCama>;
+   public idModalidad: string;
+   public idProcedimientoModalidad: string;
+   public banderaRequerido: Boolean;
+ 
+   constructor(
+     private formBuilder: FormBuilder,
+     private procedimientoService: ProcedimientoService,
+     private estadoCamaService: EstadoCamaService,
+     private utilityService: UtilityServiceService,
+     private editarComponentesService: EditarComponentesService
+   ) {
+     this.buildbusquedaForm();
+   }
+ 
+   ngOnInit(): void {
+     this.estadoCama = this.busquedaForm.get('stateBed').value;
+     this.setEstadosCama();
+     /**Carga de talbas */
+     this.utilityService.customIdModalidad.subscribe(msg => this.idModalidad = msg);
+     this.utilityService.customIdProcedimientoModalidad.subscribe(msg => this.idProcedimientoModalidad = msg);
+     this.utilityService.customIdProcedimiento.subscribe(msg => this.codigoProc = msg);
+     this.utilityService.customBanderaRequerido.subscribe(msg => this.banderaRequerido = msg);
+ 
+     /**id de procedimiento desde editar */
+     this.editarComponentesService.idProcedimiento.subscribe(value => {
+       if (value != '') {
+         this.codigoProc = value;
+         this.inputInactivo = "true";
+         this.busquedaForm.get('searchType').disable();
+         this.setProcedimiento();
+         console.log('deberia editar');
+       }
+     })
+   }
+ 
+   /**Gets y Sets */
+   public getObjProcedimiento() {
+     return this.procedimiento;
+   }
+   public getestadoCama() {
+     return this.estadoCama;
+   }
+   public getCodigoProcedimiento() {
+     if (this.procedimiento != null) {
+       return this.procedimiento.codigoProcedimiento;
+     } else {
+       return "";
+     }
+   }
+ 
+   /**Metodos de formularios */
+   private buildbusquedaForm() {
+     this.busquedaForm = this.formBuilder.group({
+       searchType: ['', [Validators.required]],
+       code: ['', []],
+       name: ['', []],
+       uciBed: ['', []],
+       bloodBank: ['', []],
+       stateBed: ['PEND', []]
+     });
+     this.busquedaForm.get('searchType').valueChanges
+       .subscribe(value => {
+         this.filtroBusqueda = value;
+         this.filtroSeleccionado = true;
+         if (value == '2' && this.valorBusqueda != '') {
+           this.setProcedimientos();
+         }
+       })
+     this.busquedaForm.get('uciBed').valueChanges
+       .subscribe(value => {
+         if (this.procedimiento != null && this.procedimiento.modalidades.length > 0) {
+           this.procedimiento.modalidades[0].camaUCI = value;
+         }
+       });
+     this.busquedaForm.get('bloodBank').valueChanges
+       .subscribe(value => {
+         if (this.procedimiento != null && this.procedimiento.modalidades.length > 0) {
+           this.procedimiento.modalidades[0].bancoSangre = value;
+         }
+       });
+     this.busquedaForm.get('stateBed').valueChanges
+       .subscribe(value => {
+         this.estadoCama = value;
+       });
+   }
+ 
+   private updateBusquedaForm() {
+     this.busquedaForm.get('code').setValue(this.procedimiento.codigoProcedimiento);
+ 
+     this.busquedaForm.get('name').setValue(this.procedimiento.nombre);
+     if (this.procedimiento.modalidades.length > 0) {
+       this.busquedaForm.get('uciBed').setValue(this.procedimiento.modalidades[0].camaUCI);
+       this.busquedaForm.get('bloodBank').setValue(this.procedimiento.modalidades[0].bancoSangre);
+       if (this.procedimiento.modalidades[0].camaUCI) {
+         this.busquedaForm.get('uciBed').disable();
+       } else {
+         this.busquedaForm.get('uciBed').enable();
+       }
+       if (this.procedimiento.modalidades[0].bancoSangre) {
+         this.busquedaForm.get('bloodBank').disable();
+       } else {
+         this.busquedaForm.get('bloodBank').enable();
+       }
+     } else {
+       this.busquedaForm.get('uciBed').setValue(false);
+       this.busquedaForm.get('bloodBank').setValue(false);
+     }
+   }
+ 
+   /**Metodos de busqueda autocompletada */
+   selectEvent(item) {
+     this.codigoProc = item.id;
+     this.setProcedimiento();
+   }
+ 
+   onChangeSearch($event) {
+     if ($event != '') {
+       this.inputVacio = true;
+       if (this.filtroBusqueda != '') {
+         if (this.filtroBusqueda == '1') {
+           this.codigoProc = $event;
+         } else {
+           this.valorBusqueda = $event;
+           this.setProcedimientos();
+         }
+       } else {
+         this.codigoProc = $event;
+         this.valorBusqueda = $event;
+       }
+     } else {
+       this.inputVacio = false;
+     }
+   }
+ 
+   cargarNombres() {
+     this.data = new Array();
+     this.procedimientos.forEach(procedimiento => {
+       this.data.push({ id: procedimiento.codigoProcedimiento, name: procedimiento.nombre });
+     });
+   }
+ 
+   /**Metodos de los botones */
+   public buscarClick() {
+     if (this.codigoProc != '' && this.filtroBusqueda != '') {
+       this.setProcedimiento();
+     } else {
+       console.log(this.codigoProc);
+       if (this.filtroBusqueda == '' && this.codigoProc == '') {
+         this.filtroSeleccionado = false;
+         this.inputVacio = false;
+       } else {
+         if (this.filtroBusqueda != '') {
+           this.inputVacio = false;
+         }
+         if (this.codigoProc != '') {
+           this.filtroSeleccionado = false;
+         }
+       }
+     }
+   }
+ 
+   /**Peticiones */
+   async setProcedimiento() {
+     let res: any = await this.procedimientoService.getCodigo(this.codigoProc).toPromise();
+     /**TODO:Capturar el error. que venga del servidor */
+     if (res != null) {
+       this.procedimiento = Procedimiento.fromJSON(res.procedimiento);
+       if (this.procedimiento != null) {
+         this.updateBusquedaForm();
+         this.idModalidad = this.procedimiento.modalidades[0].idModalidad_id.toString();
+         this.idProcedimientoModalidad = this.procedimiento.modalidades[0].idProcedimientoModalidad.toString();
+         this.utilityService.changeIdProcedimiento(this.codigoProc);
+         this.utilityService.changeIdModalidad(this.idModalidad);
+         this.utilityService.changeBanderaRequerido(this.banderaRequerido);
+       }
+       this.procedimientos = new Array<Procedimiento>();
+     } else {
+       this.msjBusquedaNula += 'codigo:' + this.codigoProc;
+       this.busquedaNula = false;
+     }
+   }
+ 
+   async setProcedimientos() {
+     let res: any = await this.procedimientoService.getNombre(this.valorBusqueda).toPromise();
+     this.procedimientos = new Array<Procedimiento>();
+     res.procedimientos.forEach(procedimiento => {
+       this.procedimientos.push(procedimiento);
+     });
+     this.procedimiento = null;
+     this.data = this.procedimientos;
+     if (this.procedimientos.length > 0) {
+       this.cargarNombres();
+     }
+   }
+ 
+   async setEstadosCama() {
+     let res: any = await this.estadoCamaService.get().toPromise();
+     this.estadosCama = EstadoCama.fromJSON(res);
+   }
+ }
+ 
